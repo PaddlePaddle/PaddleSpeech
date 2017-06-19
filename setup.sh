d35b747175e36b44c369ef9ceb2b9fd7f9cbd9ec
yangyaming <mxscmxsc@gmail.com>
2017-06-19 23:24:58 +0800

    Fix ci following:     1. Unify the dependency installation process in setup.sh.     2. Change the version of package scipy from 0.13.0b1 to 0.13.1

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# install python dependencies
+if [ -f 'requirements.txt' ]; then
+    pip install -r requirements.txt
+fi
+
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+# install scikits.samplerate
+curl -O "http://www.mega-nerd.com/SRC/libsamplerate-0.1.9.tar.gz"
+if [ $? != 0 ]; then
+    echo "Download libsamplerate-0.1.9.tar.gz failed !!!"
+    exit 1
+fi
+tar -xvf libsamplerate-0.1.9.tar.gz
+cd libsamplerate-0.1.9
+./configure && make && make install
+cd -
+rm -rf libsamplerate-0.1.9
+rm libsamplerate-0.1.9.tar.gz
+pip install scikits.samplerate==0.3.3
+if [ $? != 0 ]; then
+    echo "Install sckikits.samplerate failed !!!"
+    exit 1
+fi

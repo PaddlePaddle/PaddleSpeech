fb75f159a4b1e67a1103db26db8daf76e38559a4
Xinghai Sun <sunxinghai1216@gmail.com>
2017-09-15 19:50:59 +0800

    Publish urls for aishell model and chinese language model.

---
+++ models/aishell/download_model.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+source ../../utils/utility.sh
+
+URL='http://cloud.dlnel.org/filepub/?uuid=6c83b9d8-3255-4adf-9726-0fe0be3d0274'
+MD5=28521a58552885a81cf92a1e9b133a71
+TARGET=./aishell_model.tar.gz
+
+
+echo "Download Aishell model ..."
+download $URL $MD5 $TARGET
+if [ $? -ne 0 ]; then
+    echo "Fail to download Aishell model!"
+    exit 1
+fi
+tar -zxvf $TARGET
+
+
+exit 0
--- models/librispeech/download_model.sh
+++ models/librispeech/download_model.sh
@@ -2,7 +2,6 @@
 
 source ../../utils/utility.sh
 
-# TODO: add urls
 URL='http://cloud.dlnel.org/filepub/?uuid=17404caf-cf19-492f-9707-1fad07c19aae'
 MD5=ea5024a457a91179472f6dfee60e053d
 TARGET=./librispeech_model.tar.gz

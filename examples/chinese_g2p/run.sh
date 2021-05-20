3058068d684805d2148aee95d50cf73409cfda7c
Hui Zhang <zhtclz@foxmail.com>
2021-05-20 16:20:20 +0800

    refactor g2p egs (#630)
    * refactor g2p egs
    
    * add sha-bone; remove avg.sh from egs;

--- examples/chinese_g2p/run.sh
+++ examples/chinese_g2p/run.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
+
 source path.sh
 
 stage=-1
 stop_stage=100
 
-exp_dir="exp"
-data_dir="data"
+exp_dir=exp
+data_dir=data
+
 source ${MAIN_ROOT}/utils/parse_options.sh || exit -1
+
 mkdir -p ${exp_dir}
-bash local/prepare_dataset.sh --exp-dir ${exp_dir} --data-dir ${data_dir}
+
+
+if [ $stage -le 0 ] && [ $stop_stage -ge 0 ];then
+    echo "stage 0: Extracting Prosody Labeling"
+    bash local/prepare_dataset.sh --exp-dir ${exp_dir} --data-dir ${data_dir}
+fi
 
 # convert transcription in chinese into pinyin with pypinyin or jieba+pypinyin
 filename="000001-010000.txt"
-echo "Processing transcriptions..."
 
-python3 local/extract_pinyin_label.py ${exp_dir}/${filename} ${exp_dir}/"pinyin_baker.py"
-python3 local/convert_transcription.py ${exp_dir}/${filename} ${exp_dir}/"result_pypinyin.txt"
-python3 local/convert_transcription.py --use-jieba ${exp_dir}/${filename} ${exp_dir}/"result_pypinyin_with_jieba.txt"
+if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
+    echo "stage 1: Processing transcriptions..."
+    python3 local/extract_pinyin_label.py ${exp_dir}/${filename} ${exp_dir}/ref.pinyin
+
+    python3 local/convert_transcription.py ${exp_dir}/${filename} ${exp_dir}/trans.pinyin
+    python3 local/convert_transcription.py --use-jieba ${exp_dir}/${filename} ${exp_dir}/trans.jieba.pinyin
+fi
 
 echo "done"
 exit 0

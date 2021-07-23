d7ca8bda0971ef79932c6cdcb01828a9ca6cf868
Hui Zhang <zhtclz@foxmail.com>
2021-07-23 11:22:50 +0000

    refactor cmake

---
+++ speechnn/cmake/third_party/absl.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION 3.14)
+include(FetchContent)
+
+FetchContent_Declare(
+  absl
+  GIT_REPOSITORY "https://github.com/abseil/abseil-cpp.git"
+  GIT_TAG "20210324.1"
+)
+
+FetchContent_MakeAvailable(absl)
+
+

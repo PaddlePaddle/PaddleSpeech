c998a007f1dce113eb708e7f4cd68c9160f9c5e5
Hui Zhang <zhtclz@foxmail.com>
2021-07-26 12:08:11 +0000

    add srilm install

--- examples/aishell/s1/path.sh
+++ examples/aishell/s1/path.sh
@@ -14,13 +14,14 @@ MODEL=u2
 export BIN_DIR=${MAIN_ROOT}/deepspeech/exps/${MODEL}/bin
 
 
-export LIBLBFGS=/workspace/zhanghui/asr/wenet-210713/tools/liblbfgs-1.10
+# srilm
+export LIBLBFGS=${MAIN_ROOT}/tools/liblbfgs-1.10
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:${LIBLBFGS}/lib/.libs
-export SRILM=/workspace/zhanghui/asr/wenet-210713/tools/srilm
+export SRILM=${MAIN_ROOT}/tools/srilm
 export PATH=${PATH}:${SRILM}/bin:${SRILM}/bin/i686-m64
 
 # Kaldi
-export KALDI_ROOT=/workspace/zhanghui/asr/kaldi
+export KALDI_ROOT=${MAIN_ROOT}/tools/kaldi
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1

0f3e5a3872defc3e7197e01f8ae7e760b22c00bf
Hui Zhang <zhtclz@foxmail.com>
2021-09-16 05:59:16 +0000

    run_all with aishell/s1

--- tests/benchmark/run_all.sh
+++ tests/benchmark/run_all.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+ROOT_DIR=../../
+
+# 提供可稳定复现性能的脚本，默认在标准docker环境内py37执行：
 # collect env info
-bash ../../utils/pd_env_collect.sh
+bash ${ROOT_DIR}/utils/pd_env_collect.sh
+cat pd_env.txt
 
-
-
-# 提供可稳定复现性能的脚本，默认在标准docker环境内py37执行： paddlepaddle/paddle:latest-gpu-cuda10.1-cudnn7  paddle=2.1.2  py=37
 # 执行目录：需说明
-cd **
+pushd ${ROOT_DIR}/examples/aishell/s1
+
 # 1 安装该模型需要的依赖 (如需开启优化策略请注明)
-pip install ...
+pushd ${ROOT_DIR}/tools; make; popd
+source ${ROOT_DIR}/tools/venv/bin/activate
+pushd ${ROOT_DIR}; bash setup.sh; popd
+
+
 # 2 拷贝该模型需要数据、预训练模型
+mkdir -p exp/log
+loca/data.sh &> exp/log/data.log
+
 # 3 批量运行（如不方便批量，1，2需放到单个模型中）
 
-model_mode_list=(MobileNetv1 MobileNetv2)
-fp_item_list=(fp32 fp16)
+model_mode_list=(conformer)
+fp_item_list=(fp32)
 bs_item=(32 64 96)
 for model_mode in ${model_mode_list[@]}; do
       for fp_item in ${fp_item_list[@]}; do
@@ -31,3 +40,5 @@ for model_mode in ${model_mode_list[@]}; do
             done
       done
 done
+
+popd # aishell/s1

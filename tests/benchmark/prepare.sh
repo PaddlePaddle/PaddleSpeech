c1b4366834f4c46ddc0f44c2d0bf4e8e70252fe9
huangyuxin <hyxin2014@126.com>
2021-10-09 02:52:41 +0000

    update the installing script for openfst

--- tests/benchmark/prepare.sh
+++ tests/benchmark/prepare.sh
@@ -1,7 +1,7 @@
 source ../tools/venv/bin/activate
 
-#进入执行目录
+#Enter the example dir
 pushd ../examples/aishell/s1
 
-#准备数据
+#Prepare the data
 bash run.sh --stage 0 --stop_stage 0
--- tests/benchmark/run_benchmark.sh
+++ tests/benchmark/run_benchmark.sh
@@ -1,40 +1,50 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -xe
-
 # 运行示例：CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh ${run_mode} ${bs_item} ${fp_item} 500 ${model_mode}
 # 参数说明
 function _set_params(){
+
     run_mode=${1:-"sp"}          # 单卡sp|多卡mp
-    batch_size=${2:-"64"}
-    fp_item=${3:-"fp32"}        # fp32|fp16
-    max_iter=${4:-"500"}       # 可选，如果需要修改代码提前中断
-    model_name=${5:-"model_name"}
+    config_path=${2:-"conf/conformer.yaml"}
+    output=${3:-"exp/conformer"}
+    seed=${4:-"0"}
+    ngpu=${5:-"1"}
+    profiler_options=${6:-"None"}
+    batch_size=${7:-"32"}
+    fp_item=${8:-"fp32"}
+    TRAIN_LOG_DIR=${9:-$(pwd)}
+
+    benchmark_max_step=0
+
     run_log_path=${TRAIN_LOG_DIR:-$(pwd)}  # TRAIN_LOG_DIR 后续QA设置该参数
 
 #   以下不用修改
     device=${CUDA_VISIBLE_DEVICES//,/ }
     arr=(${device})
     num_gpu_devices=${#arr[*]}
-    log_file=${run_log_path}/${model_name}_${run_mode}_bs${batch_size}_${fp_item}_${num_gpu_devices}
+    log_file=${run_log_path}/recoder_${run_mode}_bs${batch_size}_${fp_item}_ngpu${ngpu}.txt
 }
 
 function _train(){
     echo "Train on ${num_gpu_devices} GPUs"
     echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
+    train_cmd="--config=${config_path}
+               --output=${output}
+               --seed=${seed}
+               --nproc=${ngpu}
+               --profiler-options "${profiler_options}"
+               --benchmark-batch-size ${batch_size}
+               --benchmark-max-step ${benchmark_max_step} "
 
-    train_cmd="--benchmark-batch-size ${batch_size}
-               --benchmark-max-step ${max_iter}
-               conf/${model_name}.yaml ${model_name}"
+    echo "run_mode "${run_mode}
 
     case ${run_mode} in
-    sp) train_cmd="bash local/train.sh "${train_cmd}"" ;;
-    mp)
-        train_cmd="bash local/train.sh "${train_cmd}"" ;;
+    sp) train_cmd="python3 -u ${BIN_DIR}/train.py "${train_cmd} ;;
+    mp) train_cmd="python3 -u ${BIN_DIR}/train.py "${train_cmd} ;;
     *) echo "choose run_mode(sp or mp)"; exit 1;
     esac
-
-    # 以下不用修改
+    echo ${train_cmd}
+# 以下不用修改
     timeout 15m ${train_cmd} > ${log_file} 2>&1
     if [ $? -ne 0 ];then
         echo -e "${model_name}, FAIL"
@@ -43,8 +53,7 @@ function _train(){
         echo -e "${model_name}, SUCCESS"
         export job_fail_flag=0
     fi
-
-    trap 'for pid in $(jobs -pr); do kill -KILL $pid; done' INT QUIT TERM
+    kill -9 `ps -ef|grep 'python'|awk '{print $2}'`
 
     if [ $run_mode = "mp" -a -d mylog ]; then
         rm ${log_file}
@@ -54,4 +63,3 @@ function _train(){
 
 _set_params $@
 _train
-

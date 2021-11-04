c463a00f8105b2790bf3af5e2394bc7ee04e7e3a
Hui Zhang <zhtclz@foxmail.com>
2021-11-04 03:28:28 +0000

    add reference code license

--- paddlespeech/s2t/modules/loss.py
+++ paddlespeech/s2t/modules/loss.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Reference wenet Apache 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 import inspect
 
 import paddle

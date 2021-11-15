39400e5ee8f8420a5d64d5a6acaf5226996537a7
TianYuan <white-sky@qq.com>
2021-11-15 10:42:17 +0800

    Update synthesize.sh

--- examples/ljspeech/tts0/local/synthesize.sh
+++ examples/ljspeech/tts0/local/synthesize.sh
@@ -7,5 +7,5 @@ python3 ${BIN_DIR}/synthesize.py \
     --config=${train_output_path}/config.yaml \
     --checkpoint_path=${train_output_path}/checkpoints/${ckpt_name} \
     --input=${BIN_DIR}/../sentences_en.txt \
-    --output=${train_output_path}/test
+    --output=${train_output_path}/test \
     --ngpu=1

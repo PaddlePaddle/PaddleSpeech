db0003e8dde4ea3efd3af044d6813775f7a556bc
TianYuan <white-sky@qq.com>
2021-11-16 14:26:16 +0800

    Update synthesize_e2e.sh

--- examples/aishell3/tts3/local/synthesize_e2e.sh
+++ examples/aishell3/tts3/local/synthesize_e2e.sh
@@ -10,9 +10,9 @@ python3 ${BIN_DIR}/multi_spk_synthesize_e2e.py \
   --fastspeech2-config=${config_path} \
   --fastspeech2-checkpoint=${train_output_path}/checkpoints/${ckpt_name} \
   --fastspeech2-stat=dump/train/speech_stats.npy \
-  --pwg-config=pwg_baker_ckpt_0.4/pwg_default.yaml \
-  --pwg-checkpoint=pwg_baker_ckpt_0.4/pwg_snapshot_iter_400000.pdz \
-  --pwg-stat=pwg_baker_ckpt_0.4/pwg_stats.npy \
+  --pwg-config=pwg_aishell3_ckpt_0.5/default.yaml \
+  --pwg-checkpoint=pwg_aishell3_ckpt_0.5/snapshot_iter_1000000.pdz \
+  --pwg-stat=pwg_aishell3_ckpt_0.5/feats_stats.npy  \
   --text=${BIN_DIR}/../sentences.txt \
   --output-dir=${train_output_path}/test_e2e \
   --phones-dict=dump/phone_id_map.txt \

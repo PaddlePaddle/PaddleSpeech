b5a2675e2bdce83abe406ccb3e69ed524a4753e3
WilliamZhang06 <97937340+WilliamZhang06@users.noreply.github.com>
2022-01-27 15:37:09 +0800

    [Server] added engine factory and config  (#1399)
    * added engine other works, test=server
    
    * fix comments, test=doc

--- speechserving/speechserving/restful/api.py
+++ speechserving/speechserving/restful/api.py
@@ -11,11 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import List
 from fastapi import APIRouter
 
 from .tts_api import router as tts_router
-#from .asr_api import router as asr_router
+from .asr_api import router as asr_router
 
-router = APIRouter()
-#router.include_router(asr_router)
-router.include_router(tts_router)
+_router = APIRouter()
+
+def setup_router(api_list: List):
+
+    for api_name in api_list:
+        if api_name == 'asr':
+            _router.include_router(asr_router)
+        elif api_name == 'tts':
+            _router.include_router(tts_router)
+        else:
+            pass
+
+    return _router
+

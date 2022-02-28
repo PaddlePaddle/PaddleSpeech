5f1728f8552909ec3507c65ef7157cf1c6210ee1
lym0302 <lym0302@foxmail.com>
2022-02-28 14:11:17 +0800

    rm server related, test=doc

--- paddlespeech/cli/stats/infer.py
+++ paddlespeech/cli/stats/infer.py
@@ -75,14 +75,6 @@ class StatsExecutor():
                     "Here is the list of ASR pretrained models released by PaddleSpeech that can be used by command line and python API"
                 )
                 self.show_support_models(pretrained_models)
-
-                # show ASR static pretrained model
-                from paddlespeech.server.engine.asr.paddleinference.asr_engine import pretrained_models
-                logger.info(
-                    "Here is the list of ASR static pretrained models released by PaddleSpeech that can be used by command line and python API"
-                )
-                self.show_support_models(pretrained_models)
-
                 return True
             except BaseException:
                 logger.error("Failed to get the list of ASR pretrained models.")
@@ -132,14 +124,6 @@ class StatsExecutor():
                     "Here is the list of TTS pretrained models released by PaddleSpeech that can be used by command line and python API"
                 )
                 self.show_support_models(pretrained_models)
-
-                # show TTS static pretrained model
-                from paddlespeech.server.engine.tts.paddleinference.tts_engine import pretrained_models
-                logger.info(
-                    "Here is the list of TTS static pretrained models released by PaddleSpeech that can be used by command line and python API"
-                )
-                self.show_support_models(pretrained_models)
-
                 return True
             except BaseException:
                 logger.error("Failed to get the list of TTS pretrained models.")

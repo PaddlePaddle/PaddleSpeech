3a943ca95b13818409efa6253b05fb2831ab2419
xiongxinlei <xiongxinlei@baidu.com>
2022-03-02 20:59:45 +0800

    repair the variable name bug

--- examples/voxceleb/sv0/run.sh
+++ examples/voxceleb/sv0/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 . ./path.sh
 set -e
-export PPAUDIO_HOME=/home/users/xiongxinlei/exprts/v3
+
 dir=./data/
 mkdir -p ${dir}
-# you can set the variable DATA_HOME to specifiy the downloaded the vox1 and vox2 dataset
+# you can set the variable PPAUDIO_HOME to specifiy the downloaded the vox1 and vox2 dataset
 python3 \
      local/train.py \
      --data-dir ${dir}

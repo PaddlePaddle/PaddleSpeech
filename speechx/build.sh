a89032d21b3c59aab1f7f8a238976a7cae928a26
SmileGoat <goat.zhou@qq.com>
2022-03-04 10:56:31 +0800

    add comment

--- speechx/build.sh
+++ speechx/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# the build script had verified in the paddlepaddle docker image.
+# please follow the instruction below to install PaddlePaddle image.
+# https://www.paddlepaddle.org.cn/documentation/docs/zh/install/docker/linux-docker.html
+
 boost_SOURCE_DIR=$PWD/fc_patch/boost-src
 if [ ! -d ${boost_SOURCE_DIR} ]; then
   wget https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.tar.gz 

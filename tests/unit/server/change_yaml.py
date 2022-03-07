3b304544f6187b91368c66e5a5b16840f69d175c
lym0302 <lym0302@foxmail.com>
2022-03-07 18:19:17 +0800

    modify yaml, test=doc

--- tests/unit/server/change_yaml.py
+++ tests/unit/server/change_yaml.py
@@ -5,7 +5,7 @@ import os
 import yaml
 
 
-def change_speech_yaml(yaml_name: str, device: str):
+def change_device(yamlfile: str, engine: str, device: str):
     """Change the settings of the device under the voice task configuration file
 
     Args:
@@ -13,68 +13,54 @@ def change_speech_yaml(yaml_name: str, device: str):
         cpu (bool): True means set device to "cpu"
         model_type (dict): change model type
     """
-    if "asr" in yaml_name:
-        dirpath = "./conf/asr/"
-    elif 'tts' in yaml_name:
-        dirpath = "./conf/tts/"
-    yamlfile = dirpath + yaml_name + ".yaml"
-    tmp_yamlfile = dirpath + yaml_name + "_tmp.yaml"
+    tmp_yamlfile = yamlfile.split(".yaml")[0] + "_tmp.yaml"
     os.system("cp %s %s" % (yamlfile, tmp_yamlfile))
+
+    if device == 'cpu':
+        set_device = 'cpu'
+    elif device == 'gpu':
+        set_device = 'gpu:0'
+    else:
+        print("Please set correct device: cpu or gpu.")
 
     with open(tmp_yamlfile) as f, open(yamlfile, "w+", encoding="utf-8") as fw:
         y = yaml.safe_load(f)
-        if device == 'cpu':
-            print("Set device: cpu")
-            if yaml_name == 'asr':
-                y['device'] = 'cpu'
-            elif yaml_name == 'asr_pd':
-                y['am_predictor_conf']['device'] = 'cpu'
-            elif yaml_name == 'tts':
-                y['device'] = 'cpu'
-            elif yaml_name == 'tts_pd':
-                y['am_predictor_conf']['device'] = 'cpu'
-                y['voc_predictor_conf']['device'] = 'cpu'
-        elif device == 'gpu':
-            print("Set device: gpu")
-            if yaml_name == 'asr':
-                y['device'] = 'gpu:0'
-            elif yaml_name == 'asr_pd':
-                y['am_predictor_conf']['device'] = 'gpu:0'
-            elif yaml_name == 'tts':
-                y['device'] = 'gpu:0'
-            elif yaml_name == 'tts_pd':
-                y['am_predictor_conf']['device'] = 'gpu:0'
-                y['voc_predictor_conf']['device'] = 'gpu:0'
+        if engine == 'asr_python' or engine == 'tts_python':
+            y[engine]['device'] = set_device
+        elif engine == 'asr_inference':
+            y[engine]['am_predictor_conf']['device'] = set_device
+        elif engine == 'tts_inference':
+            y[engine]['am_predictor_conf']['device'] = set_device
+            y[engine]['voc_predictor_conf']['device'] = set_device
         else:
-            print("Please set correct device: cpu or gpu.")
+            print(
+                "Please set correct engine: asr_python, tts_python, asr_inference, tts_inference."
+            )
 
-        print("The content of '%s': " % (yamlfile))
         print(yaml.dump(y, default_flow_style=False, sort_keys=False))
         yaml.dump(y, fw, allow_unicode=True)
     os.system("rm %s" % (tmp_yamlfile))
     print("Change %s successfully." % (yamlfile))
 
 
-def change_app_yaml(task: str, engine_type: str):
+def change_engine_type(yamlfile: str, engine_type):
     """Change the engine type and corresponding configuration file of the speech task in application.yaml
 
     Args:
         task (str):  asr or tts
     """
-    yamlfile = "./conf/application.yaml"
-    tmp_yamlfile = "./conf/application_tmp.yaml"
+    tmp_yamlfile = yamlfile.split(".yaml")[0] + "_tmp.yaml"
     os.system("cp %s %s" % (yamlfile, tmp_yamlfile))
+    speech_task = engine_type.split("_")[0]
+
     with open(tmp_yamlfile) as f, open(yamlfile, "w+", encoding="utf-8") as fw:
         y = yaml.safe_load(f)
-        y['engine_type'][task] = engine_type
-        path_list = ["./conf/", task, "/", task]
-        if engine_type == 'python':
-            path_list.append(".yaml")
-
-        elif engine_type == 'inference':
-            path_list.append("_pd.yaml")
-        y['engine_backend'][task] = ''.join(path_list)
-        print("The content of './conf/application.yaml': ")
+        engine_list = y['engine_list']
+        for engine in engine_list:
+            if speech_task in engine:
+                engine_list.remove(engine)
+                engine_list.append(engine_type)
+        y['engine_list'] = engine_list
         print(yaml.dump(y, default_flow_style=False, sort_keys=False))
         yaml.dump(y, fw, allow_unicode=True)
     os.system("rm %s" % (tmp_yamlfile))
@@ -84,31 +70,36 @@ def change_app_yaml(task: str, engine_type: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        '--config_file',
+        type=str,
+        default='./conf/application.yaml',
+        help='server yaml file.')
+    parser.add_argument(
         '--change_task',
         type=str,
         default=None,
         help='Change task',
         choices=[
-            'app-asr-python',
-            'app-asr-inference',
-            'app-tts-python',
-            'app-tts-inference',
-            'speech-asr-cpu',
-            'speech-asr-gpu',
-            'speech-asr_pd-cpu',
-            'speech-asr_pd-gpu',
-            'speech-tts-cpu',
-            'speech-tts-gpu',
-            'speech-tts_pd-cpu',
-            'speech-tts_pd-gpu',
+            'enginetype-asr_python',
+            'enginetype-asr_inference',
+            'enginetype-tts_python',
+            'enginetype-tts_inference',
+            'device-asr_python-cpu',
+            'device-asr_python-gpu',
+            'device-asr_inference-cpu',
+            'device-asr_inference-gpu',
+            'device-tts_python-cpu',
+            'device-tts_python-gpu',
+            'device-tts_inference-cpu',
+            'device-tts_inference-gpu',
         ],
         required=True)
     args = parser.parse_args()
 
     types = args.change_task.split("-")
-    if types[0] == "app":
-        change_app_yaml(types[1], types[2])
-    elif types[0] == "speech":
-        change_speech_yaml(types[1], types[2])
+    if types[0] == "enginetype":
+        change_engine_type(args.config_file, types[1])
+    elif types[0] == "device":
+        change_device(args.config_file, types[1], types[2])
     else:
         print("Error change task, please check change_task.")

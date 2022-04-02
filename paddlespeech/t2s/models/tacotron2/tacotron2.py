828ee1440459959e405851e43df7e0037c801be2
TianYuan <white-sky@qq.com>
2022-04-02 02:31:52 +0000

    add license and reference for some models, test=doc

--- paddlespeech/t2s/models/tacotron2/tacotron2.py
+++ paddlespeech/t2s/models/tacotron2/tacotron2.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Modified from espnet(https://github.com/espnet/espnet)
 """Tacotron 2 related modules for paddle"""
 import logging
 from typing import Dict

77fd339f33f83fd2fc901afee34c7061f0e5fc51
Yang Zhou <goat.zhou@qq.com>
2022-04-22 09:48:22 +0800

    fix websocket client early return issue

--- speechx/examples/ds2_ol/websocket/websocket_client_main.cc
+++ speechx/examples/ds2_ol/websocket/websocket_client_main.cc
@@ -74,9 +74,7 @@ int main(int argc, char* argv[]) {
         std::string result = client.GetResult();
         LOG(INFO) << "utt: " << utt << " " << result;
 
-
         client.Join();
-        return 0;
     }
     return 0;
 }

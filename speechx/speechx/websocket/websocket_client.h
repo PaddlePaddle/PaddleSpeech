a3eaf16f848ed216eb4e3d386ca61cc2ae812cdf
Yang Zhou <goat.zhou@qq.com>
2022-05-06 11:41:30 +0800

    fix copyright issue

--- speechx/speechx/websocket/websocket_client.h
+++ speechx/speechx/websocket/websocket_client.h
@@ -1,5 +1,5 @@
-// Copyright (c) 2022 PaddlePaddle Wenet Authors. All Rights Reserved.
-//
+// Copyright (c) 2020 Mobvoi Inc (Binbin Zhang)
+//               2022 PaddlePaddle Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at

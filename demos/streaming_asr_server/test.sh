5656768c9508894965ccfde4e6374b0a8d6364be
Hui Zhang <zhtclz@foxmail.com>
2022-05-10 10:10:04 +0800

    streaming asr server using paddlespeech_server/client

--- demos/streaming_asr_server/test.sh
+++ demos/streaming_asr_server/test.sh
@@ -2,7 +2,9 @@
 wget -c https://paddlespeech.bj.bcebos.com/PaddleAudio/zh.wav 
 
 # read the wav and pass it to only streaming asr service
-python3 websocket_client.py --server_ip 127.0.0.1 --port 8290 --wavfile ./zh.wav
+# python3 websocket_client.py --server_ip 127.0.0.1 --port 8290 --wavfile ./zh.wav
+paddlespeech_client asr_online --server_ip 127.0.0.1 --port 8290 --input ./zh.wav
 
 # read the wav and call streaming and punc service
-python3 websocket_client.py --server_ip 127.0.0.1 --port 8290 --punc.server_ip 127.0.0.1 --punc.port 8190 --wavfile ./zh.wav
+# python3 websocket_client.py --server_ip 127.0.0.1 --port 8290 --punc.server_ip 127.0.0.1 --punc.port 8190 --wavfile ./zh.wav
+paddlespeech_client asr_online --server_ip 127.0.0.1 --port 8290 --punc.server_ip 127.0.0.1 --punc.port 8190 --input ./zh.wav

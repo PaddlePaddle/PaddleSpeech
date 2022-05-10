2381d2b078ad31cdbc866a7d08bcd412c3a820ae
root <root@yq01-sys-hic-k8s-v100-qm-a225-0004.yq01.baidu.com>
2022-05-10 11:00:52 +0000

    add cacu rtf scripts, test=doc

---
+++ tests/unit/cli/cacu_rtf_by_aishell.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+stage=-1
+stop_stage=100
+MAIN_ROOT=../../..
+
+. ${MAIN_ROOT}/utils/parse_options.sh || exit -1;
+TARGET_DIR=${MAIN_ROOT}/dataset
+mkdir -p ${TARGET_DIR}
+mkdir -p data
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    # download data, generate manifests
+    python3 aishell_test_prepare.py \
+    --manifest_prefix="data/manifest" \
+    --target_dir="${TARGET_DIR}/aishell"
+
+    if [ $? -ne 0 ]; then
+        echo "Prepare Aishell failed. Terminated."
+        exit 1
+    fi
+
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+   cat data/manifest.test | paddlespeech asr --model conformer_online_aishell --rtf -v
+fi

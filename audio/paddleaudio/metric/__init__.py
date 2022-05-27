662eb3fbb83023d243e040e98093c7695253a47d
KP <109694228@qq.com>
2022-05-27 20:19:40 +0800

    Update __init__.py

--- audio/paddleaudio/metric/__init__.py
+++ audio/paddleaudio/metric/__init__.py
@@ -11,6 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .dtw import dtw_distance
 from .eer import compute_eer
 from .eer import compute_minDCF

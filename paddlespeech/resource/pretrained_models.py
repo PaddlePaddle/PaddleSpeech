42d28b961ca16adf3f0e7280bd6f16d8fd11c8f3
Hui Zhang <zhtclz@foxmail.com>
2022-06-16 05:00:30 +0000

    fix pretrian model error

--- paddlespeech/resource/pretrained_models.py
+++ paddlespeech/resource/pretrained_models.py
@@ -170,14 +170,22 @@ asr_dynamic_pretrained_models = {
         '1.0.2': {
             'url':
             'http://paddlespeech.bj.bcebos.com/s2t/aishell/asr0/asr0_deepspeech2_online_aishell_fbank161_ckpt_1.0.2.model.tar.gz',
-            'md5': '4dd42cfce9aaa54db0ec698da6c48ec5',
-            'cfg_path': 'model.yaml',
-            'ckpt_path':'exp/deepspeech2_online/checkpoints/avg_1',
-            'model':'exp/deepspeech2_online/checkpoints/avg_1.jit.pdmodel',
-            'params':'exp/deepspeech2_online/checkpoints/avg_1.jit.pdiparams',
-            'onnx_model': 'onnx/model.onnx'
-            'lm_url':'https://deepspeech.bj.bcebos.com/zh_lm/zh_giga.no_cna_cmn.prune01244.klm',
-            'lm_md5':'29e02312deb2e59b3c8686c7966d4fe3'
+            'md5':
+            '4dd42cfce9aaa54db0ec698da6c48ec5',
+            'cfg_path':
+            'model.yaml',
+            'ckpt_path':
+            'exp/deepspeech2_online/checkpoints/avg_1',
+            'model':
+            'exp/deepspeech2_online/checkpoints/avg_1.jit.pdmodel',
+            'params':
+            'exp/deepspeech2_online/checkpoints/avg_1.jit.pdiparams',
+            'onnx_model':
+            'onnx/model.onnx',
+            'lm_url':
+            'https://deepspeech.bj.bcebos.com/zh_lm/zh_giga.no_cna_cmn.prune01244.klm',
+            'lm_md5':
+            '29e02312deb2e59b3c8686c7966d4fe3'
         },
     },
     "deepspeech2offline_librispeech-en-16k": {
@@ -241,32 +249,47 @@ asr_static_pretrained_models = {
         '1.0.2': {
             'url':
             'http://paddlespeech.bj.bcebos.com/s2t/aishell/asr0/asr0_deepspeech2_online_aishell_fbank161_ckpt_1.0.2.model.tar.gz',
-            'md5': '4dd42cfce9aaa54db0ec698da6c48ec5',
-            'cfg_path': 'model.yaml',
-            'ckpt_path':'exp/deepspeech2_online/checkpoints/avg_1',
-            'model':'exp/deepspeech2_online/checkpoints/avg_1.jit.pdmodel',
-            'params':'exp/deepspeech2_online/checkpoints/avg_1.jit.pdiparams',
-            'onnx_model': 'onnx/model.onnx'
-            'lm_url':'https://deepspeech.bj.bcebos.com/zh_lm/zh_giga.no_cna_cmn.prune01244.klm',
-            'lm_md5':'29e02312deb2e59b3c8686c7966d4fe3'
+            'md5':
+            '4dd42cfce9aaa54db0ec698da6c48ec5',
+            'cfg_path':
+            'model.yaml',
+            'ckpt_path':
+            'exp/deepspeech2_online/checkpoints/avg_1',
+            'model':
+            'exp/deepspeech2_online/checkpoints/avg_1.jit.pdmodel',
+            'params':
+            'exp/deepspeech2_online/checkpoints/avg_1.jit.pdiparams',
+            'onnx_model':
+            'onnx/model.onnx',
+            'lm_url':
+            'https://deepspeech.bj.bcebos.com/zh_lm/zh_giga.no_cna_cmn.prune01244.klm',
+            'lm_md5':
+            '29e02312deb2e59b3c8686c7966d4fe3'
         },
     },
 }
-
 
 asr_onnx_pretrained_models = {
     "deepspeech2online_aishell-zh-16k": {
         '1.0.2': {
             'url':
             'http://paddlespeech.bj.bcebos.com/s2t/aishell/asr0/asr0_deepspeech2_online_aishell_fbank161_ckpt_1.0.2.model.tar.gz',
-            'md5': '4dd42cfce9aaa54db0ec698da6c48ec5',
-            'cfg_path': 'model.yaml',
-            'ckpt_path':'exp/deepspeech2_online/checkpoints/avg_1',
-            'model':'exp/deepspeech2_online/checkpoints/avg_1.jit.pdmodel',
-            'params':'exp/deepspeech2_online/checkpoints/avg_1.jit.pdiparams',
-            'onnx_model': 'onnx/model.onnx'
-            'lm_url':'https://deepspeech.bj.bcebos.com/zh_lm/zh_giga.no_cna_cmn.prune01244.klm',
-            'lm_md5':'29e02312deb2e59b3c8686c7966d4fe3'
+            'md5':
+            '4dd42cfce9aaa54db0ec698da6c48ec5',
+            'cfg_path':
+            'model.yaml',
+            'ckpt_path':
+            'exp/deepspeech2_online/checkpoints/avg_1',
+            'model':
+            'exp/deepspeech2_online/checkpoints/avg_1.jit.pdmodel',
+            'params':
+            'exp/deepspeech2_online/checkpoints/avg_1.jit.pdiparams',
+            'onnx_model':
+            'onnx/model.onnx',
+            'lm_url':
+            'https://deepspeech.bj.bcebos.com/zh_lm/zh_giga.no_cna_cmn.prune01244.klm',
+            'lm_md5':
+            '29e02312deb2e59b3c8686c7966d4fe3'
         },
     },
 }

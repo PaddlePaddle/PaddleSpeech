a9a3cb4860b14b13b244ea7e7fdf013084d34fbc
Hui Zhang <zhtclz@foxmail.com>
2022-07-05 09:04:01 +0000

    remove fluid tools for onnx export,test=doc

--- speechx/examples/ds2_ol/onnx/run.sh
+++ speechx/examples/ds2_ol/onnx/run.sh
@@ -39,41 +39,10 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ];then
     popd
 fi
 
-output_names=softmax_0.tmp_0,tmp_5,concat_0.tmp_0,concat_1.tmp_0
-if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ];then
-    #  prune model by outputs
-    mkdir -p $exp/prune
-
-    # prune model deps on output_names.
-    ./local/prune.sh $dir $model $param  $output_names  $exp/prune
-fi
-
-# aishell rnn hidden is 1024
-# wenetspeech rnn hiddn is 2048
-if [ $model_type == 'aishell' ];then
-    input_shape_dict="{'audio_chunk':[1,-1,161], 'audio_chunk_lens':[1], 'chunk_state_c_box':[5, 1, 1024], 'chunk_state_h_box':[5,1,1024]}"
-elif [ $model_type == 'wenetspeech' ];then
-    input_shape_dict="{'audio_chunk':[1,-1,161], 'audio_chunk_lens':[1], 'chunk_state_c_box':[5, 1, 2048], 'chunk_state_h_box':[5,1,2048]}"
-else
-    echo "not support: $model_type"
-    exit -1
-fi
-if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ];then
-    # infer shape by new shape
-    mkdir -p $exp/shape
-    echo $input_shape_dict
-    python3 local/pd_infer_shape.py \
-        --model_dir $dir \
-        --model_filename $model \
-        --params_filename $param \
-        --save_dir $exp/shape \
-        --input_shape_dict="${input_shape_dict}"
-fi
-
 input_file=$exp/static_ds2online_inputs.pickle
 test -e $input_file
 
-if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ];then
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ];then
     # to onnx
    ./local/tonnx.sh $dir $model $param $exp/model.onnx
 
@@ -81,7 +50,7 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ];then
 fi
 
 
-if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ] ;then
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ] ;then
     # ort graph optmize
     ./local/ort_opt.py --model_in $exp/model.onnx --opt_level 0 --model_out $exp/model.ort.opt.onnx
 
@@ -89,7 +58,7 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ] ;then
 fi
 
 
-if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ];then
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ];then
     # convert opset_num to 11
     ./local/onnx_convert_opset.py --target-opset 11  --model-file $exp/model.ort.opt.onnx --save-model $exp/model.optset11.onnx 
 

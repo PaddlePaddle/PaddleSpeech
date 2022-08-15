060fd947802f3b8c621b2dae59f8f36c038b41bc
TianYuan <white-sky@qq.com>
2022-08-15 10:25:57 +0000

    format g2pw

--- paddlespeech/t2s/frontend/g2pw/utils.py
+++ paddlespeech/t2s/frontend/g2pw/utils.py
@@ -1,10 +1,22 @@
-
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Credits
- This code is modified from https://github.com/GitYCC/g2pW
+    This code is modified from https://github.com/GitYCC/g2pW
 """
 import re
-import sys
+
 
 def wordize_and_map(text):
     words = []
@@ -92,7 +104,6 @@ default_config_dict = {
         'char-linear': True,
         'pos-linear': False,
         'char+pos-second': True,
-
         'char+pos-second_lowrank': False,
         'lowrank_size': 0,
         'char+pos-second_fm': False,

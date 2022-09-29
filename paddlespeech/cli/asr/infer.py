853238d7f8941552323f41c5db14095e9d5894a2
ZapBird <zhaolx5@asiainfo.com>
2022-09-29 20:33:46 +0800

    solve Reorder python imports Failed.

--- paddlespeech/cli/asr/infer.py
+++ paddlespeech/cli/asr/infer.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import argparse
+import io
 import os
 import sys
 import time
-import io 
 from collections import OrderedDict
 from typing import List
 from typing import Optional

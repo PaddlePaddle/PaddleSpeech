0d45094b3a94ebba4e658b7a817493f979854c87
tianhao zhang <15600919271@163.com>
2022-10-31 06:39:50 +0000

    wav2vec2 license

--- paddlespeech/s2t/models/wav2vec2/modules/activations.py
+++ paddlespeech/s2t/models/wav2vec2/modules/activations.py
@@ -1,4 +1,8 @@
-# Copyright 2020 The HuggingFace Team. All rights reserved.
+# Authors
+#  * Mirco Ravanelli 2020
+#  * Jianyuan Zhong 2020
+# """
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Modified from speechbrain(https://github.com/speechbrain/speechbrain/blob/develop/speechbrain/nnet/activations.py).
 import math
 
 from paddle import nn
--- paddlespeech/s2t/models/wav2vec2/modules/containers.py
+++ paddlespeech/s2t/models/wav2vec2/modules/containers.py
@@ -1,7 +1,22 @@
+# Authors
+#  * Peter Plantinga 2020
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Modified from speechbrain(https://github.com/speechbrain/speechbrain/blob/develop/speechbrain/nnet/containers.py).
 import inspect
 
 import paddle
-
 
 class Sequential(paddle.nn.LayerDict):
     """A sequence of modules with potentially inferring shape on construction.

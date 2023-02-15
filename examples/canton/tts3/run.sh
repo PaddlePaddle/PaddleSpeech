71bda2443798f3084002ca6609e47b54861f0f8b
lance6716 <lance6716@gmail.com>
2023-02-15 12:59:38 +0800

    [TTS]Fix canton (#2924)
    * Update run.sh
    
    * Update README.md

--- examples/canton/tts3/run.sh
+++ examples/canton/tts3/run.sh
@@ -35,36 +35,3 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
     # synthesize_e2e, vocoder is pwgan by default
     CUDA_VISIBLE_DEVICES=${gpus} ./local/synthesize_e2e.sh ${conf_path} ${train_output_path} ${ckpt_name} || exit -1
 fi
-
-if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    # inference with static model, vocoder is pwgan by default
-    CUDA_VISIBLE_DEVICES=${gpus} ./local/inference.sh ${train_output_path} || exit -1
-fi
-
-if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
-    # install paddle2onnx
-    version=$(echo `pip list |grep "paddle2onnx"` |awk -F" " '{print $2}')
-    if [[ -z "$version" || ${version} != '1.0.0' ]]; then
-        pip install paddle2onnx==1.0.0
-    fi
-    ./local/paddle2onnx.sh ${train_output_path} inference inference_onnx fastspeech2_aishell3
-    # considering the balance between speed and quality, we recommend that you use hifigan as vocoder
-    ./local/paddle2onnx.sh ${train_output_path} inference inference_onnx pwgan_aishell3
-    # ./local/paddle2onnx.sh ${train_output_path} inference inference_onnx hifigan_aishell3
-    
-fi
-
-# inference with onnxruntime, use fastspeech2 + pwgan by default
-if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
-    ./local/ort_predict.sh ${train_output_path}
-fi
-
-if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
-    ./local/export2lite.sh ${train_output_path} inference pdlite fastspeech2_aishell3 x86
-    ./local/export2lite.sh ${train_output_path} inference pdlite pwgan_aishell3 x86
-    # ./local/export2lite.sh ${train_output_path} inference pdlite hifigan_aishell3 x86
-fi
-
-if [ ${stage} -le 8 ] && [ ${stop_stage} -ge 8 ]; then
-    CUDA_VISIBLE_DEVICES=${gpus} ./local/lite_predict.sh ${train_output_path} || exit -1
-fi

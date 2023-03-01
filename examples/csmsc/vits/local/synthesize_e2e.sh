16d75eaa52040a5b1abb65e3f5ea20d0e6a089f6
TianYuan <white-sky@qq.com>
2023-03-01 10:21:33 +0800

    Update synthesize_e2e.sh

--- examples/csmsc/vits/local/synthesize_e2e.sh
+++ examples/csmsc/vits/local/synthesize_e2e.sh
@@ -13,6 +13,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     FLAGS_allocator_strategy=naive_best_fit \
     FLAGS_fraction_of_gpu_memory_to_use=0.01 \
     python3 ${BIN_DIR}/synthesize_e2e.py \
+        --am=vits_csmsc \
         --config=${config_path} \
         --ckpt=${train_output_path}/checkpoints/${ckpt_name} \
         --phones_dict=dump/phone_id_map.txt \

96038fb01ba882180e64786947f5694e28d0b035
TianYuan <white-sky@qq.com>
2023-03-14 02:53:22 +0000

    fix TTSArmLinux

--- demos/TTSArmLinux/src/main.cc
+++ demos/TTSArmLinux/src/main.cc
@@ -1,23 +1,48 @@
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <front/front_interface.h>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <paddle_api.h>
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <string>
-#include <map>
-#include <glog/logging.h>
-#include <gflags/gflags.h>
-#include <paddle_api.h>
-#include <front/front_interface.h>
 #include "Predictor.hpp"
 
 using namespace paddle::lite_api;
 
-DEFINE_string(sentence, "你好，欢迎使用语音合成服务", "Text to be synthesized (Chinese only. English will crash the program.)");
+DEFINE_string(
+    sentence,
+    "你好，欢迎使用语音合成服务",
+    "Text to be synthesized (Chinese only. English will crash the program.)");
 DEFINE_string(front_conf, "./front.conf", "Front configuration file");
-DEFINE_string(acoustic_model, "./models/cpu/fastspeech2_csmsc_arm.nb", "Acoustic model .nb file");
-DEFINE_string(vocoder, "./models/cpu/fastspeech2_csmsc_arm.nb", "vocoder .nb file");
+DEFINE_string(acoustic_model,
+              "./models/cpu/fastspeech2_csmsc_arm.nb",
+              "Acoustic model .nb file");
+DEFINE_string(vocoder,
+              "./models/cpu/fastspeech2_csmsc_arm.nb",
+              "vocoder .nb file");
 DEFINE_string(output_wav, "./output/tts.wav", "Output WAV file");
-DEFINE_string(wav_bit_depth, "16", "WAV bit depth, 16 (16-bit PCM) or 32 (32-bit IEEE float)");
-DEFINE_string(wav_sample_rate, "24000", "WAV sample rate, should match the output of the vocoder");
+DEFINE_string(wav_bit_depth,
+              "16",
+              "WAV bit depth, 16 (16-bit PCM) or 32 (32-bit IEEE float)");
+DEFINE_string(wav_sample_rate,
+              "24000",
+              "WAV sample rate, should match the output of the vocoder");
 DEFINE_string(cpu_thread, "1", "CPU thread numbers");
 
 int main(int argc, char *argv[]) {
@@ -53,7 +78,7 @@ int main(int argc, char *argv[]) {
 
     // 繁体转简体
     std::wstring sentence_simp;
-    front_inst->Trand2Simp(ws_sentence, sentence_simp); 
+    front_inst->Trand2Simp(ws_sentence, &sentence_simp);
     ws_sentence = sentence_simp;
 
     std::string s_sentence;
@@ -63,28 +88,30 @@ int main(int argc, char *argv[]) {
 
     // 根据标点进行分句
     LOG(INFO) << "Start to segment sentences by punctuation";
-    front_inst->SplitByPunc(ws_sentence, sentence_part); 
+    front_inst->SplitByPunc(ws_sentence, &sentence_part);
     LOG(INFO) << "Segment sentences through punctuation successfully";
 
     // 分句后获取音素id
-    LOG(INFO) << "Start to get the phoneme and tone id sequence of each sentence";
-    for(int i = 0; i < sentence_part.size(); i++) {
-
-        LOG(INFO) << "Raw sentence is: " << ppspeech::wstring2utf8string(sentence_part[i]);
-        front_inst->SentenceNormalize(sentence_part[i]);
+    LOG(INFO)
+        << "Start to get the phoneme and tone id sequence of each sentence";
+    for (int i = 0; i < sentence_part.size(); i++) {
+        LOG(INFO) << "Raw sentence is: "
+                  << ppspeech::wstring2utf8string(sentence_part[i]);
+        front_inst->SentenceNormalize(&sentence_part[i]);
         s_sentence = ppspeech::wstring2utf8string(sentence_part[i]);
         LOG(INFO) << "After normalization sentence is: " << s_sentence;
-        
-        if (0 != front_inst->GetSentenceIds(s_sentence, phoneids, toneids)) {
+
+        if (0 != front_inst->GetSentenceIds(s_sentence, &phoneids, &toneids)) {
             LOG(ERROR) << "TTS inst get sentence phoneids and toneids failed";
             return -1;
         }
-            
     }
-    LOG(INFO) << "The phoneids of the sentence is: " << limonp::Join(phoneids.begin(), phoneids.end(), " ");
-    LOG(INFO) << "The toneids of the sentence is: " << limonp::Join(toneids.begin(), toneids.end(), " ");
+    LOG(INFO) << "The phoneids of the sentence is: "
+              << limonp::Join(phoneids.begin(), phoneids.end(), " ");
+    LOG(INFO) << "The toneids of the sentence is: "
+              << limonp::Join(toneids.begin(), toneids.end(), " ");
     LOG(INFO) << "Get the phoneme id sequence of each sentence successfully";
- 
+
 
     /////////////////////////// 后端：音素转音频 ///////////////////////////
 
@@ -99,13 +126,19 @@ int main(int argc, char *argv[]) {
     // CPU电源模式
     const PowerMode cpuPowerMode = PowerMode::LITE_POWER_HIGH;
 
-    if (!predictor->Init(FLAGS_acoustic_model, FLAGS_vocoder, cpuPowerMode, cpuThreadNum, wavSampleRate)) {
+    if (!predictor->Init(FLAGS_acoustic_model,
+                         FLAGS_vocoder,
+                         cpuPowerMode,
+                         cpuThreadNum,
+                         wavSampleRate)) {
         LOG(ERROR) << "predictor init failed" << std::endl;
         return -1;
     }
 
     std::vector<int64_t> phones(phoneids.size());
-    std::transform(phoneids.begin(), phoneids.end(), phones.begin(), [](int x) { return static_cast<int64_t>(x); });
+    std::transform(phoneids.begin(), phoneids.end(), phones.begin(), [](int x) {
+        return static_cast<int64_t>(x);
+    });
 
     if (!predictor->RunModel(phones)) {
         LOG(ERROR) << "predictor run model failed" << std::endl;
@@ -113,7 +146,8 @@ int main(int argc, char *argv[]) {
     }
 
     LOG(INFO) << "Inference time: " << predictor->GetInferenceTime() << " ms, "
-              << "WAV size (without header): " << predictor->GetWavSize() << " bytes, "
+              << "WAV size (without header): " << predictor->GetWavSize()
+              << " bytes, "
               << "WAV duration: " << predictor->GetWavDuration() << " ms, "
               << "RTF: " << predictor->GetRTF() << std::endl;
 

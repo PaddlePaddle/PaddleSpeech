882e17f67cb12baa3f3cda2671f1104dcaafe7b2
liangym <lym0302@foxmail.com>
2023-03-14 03:18:04 +0000

    add opencpop voc, test=tts

---
+++ examples/opencpop/voc1/local/PTQ_static.sh
@@ -0,0 +1,8 @@
+train_output_path=$1
+model_name=$2
+
+python3 ${BIN_DIR}/../../PTQ_static.py \
+    --dev-metadata=dump/dev/raw/metadata.jsonl \
+    --inference_dir ${train_output_path}/inference \
+    --model_name ${model_name} \
+    --onnx_format=True 
---
+++ examples/opencpop/voc1/local/synthesize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+config_path=$1
+train_output_path=$2
+ckpt_name=$3
+
+FLAGS_allocator_strategy=naive_best_fit \
+FLAGS_fraction_of_gpu_memory_to_use=0.01 \
+python3 ${BIN_DIR}/../synthesize.py \
+    --config=${config_path} \
+    --checkpoint=${train_output_path}/checkpoints/${ckpt_name} \
+    --test-metadata=dump/test/norm/metadata.jsonl \
+    --output-dir=${train_output_path}/test \
+    --generator-type=pwgan

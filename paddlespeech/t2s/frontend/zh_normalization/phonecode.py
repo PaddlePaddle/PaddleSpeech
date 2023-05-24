1f7eabee0f50f3b15eacb3b1fddc85f25edf7061
shuishu <990941859@qq.com>
2023-05-24 11:06:26 +0800

    Update phonecode.py
     # 固话的正则 错误修改
    参考https://github.com/speechio/chinese_text_normalization/blob/master/python/cn_tn.py
    固化的正则为：
     pattern = re.compile(r"\D((0(10|2[1-3]|[3-9]\d{2})-?)?[1-9]\d{6,7})\D")

--- paddlespeech/t2s/frontend/zh_normalization/phonecode.py
+++ paddlespeech/t2s/frontend/zh_normalization/phonecode.py
@@ -24,7 +24,7 @@ from .num import verbalize_digit
 RE_MOBILE_PHONE = re.compile(
     r"(?<!\d)((\+?86 ?)?1([38]\d|5[0-35-9]|7[678]|9[89])\d{8})(?!\d)")
 RE_TELEPHONE = re.compile(
-    r"(?<!\d)((0(10|2[1-3]|[3-9]\d{2})-?)?[1-9]\d{7,8})(?!\d)")
+    r"(?<!\d)((0(10|2[1-3]|[3-9]\d{2})-?)?[1-9]\d{6,7})(?!\d)")
 
 # 全国统一的号码400开头
 RE_NATIONAL_UNIFORM_NUMBER = re.compile(r"(400)(-)?\d{3}(-)?\d{4}")

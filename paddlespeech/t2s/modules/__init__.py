8b5608741c5ba72431cc6aaffaaf2969e33748bc
drryanhuang <zihaohuang@aliyun.com>
2024-12-11 04:27:22 +0000

    add fft_conv1d

--- paddlespeech/t2s/modules/__init__.py
+++ paddlespeech/t2s/modules/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .conv import *
+from .fftconv1d import *
 from .geometry import *
 from .losses import *
 from .positional_encoding import *

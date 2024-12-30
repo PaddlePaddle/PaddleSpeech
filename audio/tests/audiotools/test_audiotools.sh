09efada04e7cd2671bfbf2576ef52977bea4c096
drryanhuang <zihaohuang@aliyun.com>
2024-12-30 04:21:03 +0000

    add tar -zxvf

--- audio/tests/audiotools/test_audiotools.sh
+++ audio/tests/audiotools/test_audiotools.sh
@@ -1,4 +1,6 @@
 python -m pip install -r ../audiotools/requirements.txt
 wget  https://paddlespeech.bj.bcebos.com/PaddleAudio/audio_tools/audio.tar.gz
 wget  https://paddlespeech.bj.bcebos.com/PaddleAudio/audio_tools/regression.tar.gz
+tar -zxvf audio.tar.gz
+tar -zxvf regression.tar.gz
 python -m pytest

8509df9055c972ed12719d3c7342d46bf5d371fe
drryanhuang <zihaohuang@aliyun.com>
2024-12-30 03:48:16 +0000

    fix CI download path

--- audio/tests/audiotools/test_audiotools.sh
+++ audio/tests/audiotools/test_audiotools.sh
@@ -1,4 +1,4 @@
 python -m pip install -r ../audiotools/requirements.txt
-wget -P ./test_data https://paddlespeech.bj.bcebos.com/PaddleAudio/audio_tools/audio.tar.gz
-wget -P ./test_data https://paddlespeech.bj.bcebos.com/PaddleAudio/audio_tools/regression.tar.gz
-find . -name "*✅.py" | xargs python -m pytest
+wget  https://paddlespeech.bj.bcebos.com/PaddleAudio/audio_tools/audio.tar.gz
+wget  https://paddlespeech.bj.bcebos.com/PaddleAudio/audio_tools/regression.tar.gz
+python -m pytest

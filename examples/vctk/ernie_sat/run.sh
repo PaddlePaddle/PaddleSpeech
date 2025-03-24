1485910b1480267cda3cd2d32ddf0de156d1ffae
nyx-c-language <nyxchaoji123@163.com>
2025-03-24 15:18:36 +0800

    fix the errors found after the review

--- examples/vctk/ernie_sat/run.sh
+++ examples/vctk/ernie_sat/run.sh
@@ -27,11 +27,11 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    # synthesize, vocoder is hifigan by default stage 0 
+    # synthesize, vocoder is hifigan by default
     CUDA_VISIBLE_DEVICES=${gpus} ./local/synthesize.sh --stage 0 ${conf_path} ${train_output_path} ${ckpt_name} || exit -1
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    # synthesize, vocoder is pwgan by default stage 0, stage 1 will use hifigan as vocoder
+    # synthesize, task_name is speech synthesize by default stage 0, stage 1 will use speech edit as taskname
     CUDA_VISIBLE_DEVICES=${gpus} ./local/synthesize_e2e.sh --stage 0 ${conf_path} ${train_output_path} ${ckpt_name} || exit -1
 fi

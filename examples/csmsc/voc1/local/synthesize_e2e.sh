c70eeba0d0233712b1b5f992fe20160f77be0d86
zxcd <228587199@qq.com>
2025-05-22 09:11:52 +0000

    fix

--- examples/csmsc/voc1/local/synthesize_e2e.sh
+++ examples/csmsc/voc1/local/synthesize_e2e.sh
@@ -18,5 +18,5 @@ python3 ${BIN_DIR}/../../synthesize_e2e.py \
     --lang=zh \
     --text=${BIN_DIR}/../../../assets/sentences.txt \
     --output_dir=${train_output_path}/test_e2e \
-    --phones_dict=dump/phone_id_map.txt \
+    --phones_dict=fastspeech2_nosil_baker_ckpt_0.4/phone_id_map.txt \
     --inference_dir=${train_output_path}/inference || exit -1
